66cec1042390b4dd95f5520d970a8f8c2eb69fd5
MerkucioZemba <merkucio.sama@gmail.com>
2023-04-21 22:11:42 +0300

    Создал компоненты Header и Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>This is FOOTER</div>
+   )
+}
+export default Footer;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>This is HEADER</div>
+   )
+}
+export default Header;
