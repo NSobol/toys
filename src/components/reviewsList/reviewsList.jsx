f8a4bccfacc52f7aad34db5a51d00fcd2c7113b2
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-05-16 17:30:39 +0300

    Поправлены стили компонентов

--- src/components/reviewsList/reviewsList.jsx
+++ src/components/reviewsList/reviewsList.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Review } from '../review/rewiew';
+import style from "./reviewsList.module.css"
 
 export const ReviewsList = ({ productId, reviews = [] }) => {
   return (
     <div>
       {reviews.map((item) => (
-        <div className='review__item'>
+        <div className={style.review__item}>
           <Review productId={productId} key={item.updated_at} review={item} />{' '}
-          <button
-            onClick={() => console.log(`Удаление ${productId} ${item._id}`)}
-            className='delRew'
+			  <button
+				  onClick={() => console.log(`Удаление ${productId} ${item._id}`)}
+				  className={ style.delReview}
           >
-            Удалить отзыв
           </button>
         </div>
       ))}
