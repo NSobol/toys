414e51459dfc0f26814788a199f4ae3c5273fd08
MerkucioZemba <merkucio.sama@gmail.com>
2023-05-18 22:37:35 +0300

    Добавил ссылки модальным окнам

--- src/components/headers/registration/Registr.jsx
+++ src/components/headers/registration/Registr.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { api } from './../../../utils/api';
-import './registrStyle.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { api } from "./../../../utils/api";
+import "./registrStyle.css";
+import { Link } from "react-router-dom";
 
 export const Registr = () => {
   const {
@@ -15,53 +16,56 @@ export const Registr = () => {
     api.getRegisteredUser(data);
   };
   return (
-<div className="createModalForm">
+    <div className="createModalForm">
       <h2 className="createModalFormTitle">Регистрация</h2>
       <form className="modalForm" onSubmit={handleSubmit(onSubmit)}>
         <div className="field-form-div">
           <label className="text-form-div"> Ваш email: </label>
-        <input
-        className="form-field-input"
-          type='email'
-          id='email'
-          name='email'
-          placeholder='Email'
-          {...register('email', { required: true })}
-        />
-        {errors.email && (
-          <span style={{ color: 'red' }}>*Email* is mandatory </span>
-        )}
+          <input
+            className="form-field-input"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <span style={{ color: "red" }}>*Email* is mandatory </span>
+          )}
         </div>
         <br />
         <div className="field-form-div">
-        <label className="text-form-div"> Пароль: </label>
-        <input
-        className="form-field-input"
-          type='password'
-          id='password'
-          name='password'
-          placeholder='Пароль'
-          {...register('password', { required: true })}
-        />
+          <label className="text-form-div"> Пароль: </label>
+          <input
+            className="form-field-input"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Пароль"
+            {...register("password", { required: true })}
+          />
         </div>
-        <br /> 
+        <br />
         <input
-          className='hidden'
-          type='text'
-          id='group'
-          name='group'
-          value='group-12'
-          {...register('group')}
+          className="hidden"
+          type="text"
+          id="group"
+          name="group"
+          value="group-12"
+          {...register("group")}
         />
-         <div className="button-form-duble-two">
-        <button type="submit" className="btn button-form-submit">
-          Ok
-        </button>
-        <button type="reset" className="btn button-form-close">
-          Очистить
-        </button>
+        <div className="button-form-duble-two">
+          <button type="submit" className="btn button-form-submit">
+            Ok
+          </button>
+          <button type="reset" className="btn button-form-close">
+            Очистить
+          </button>
         </div>
       </form>
+      <div className="relocation">
+        <span>Есть аккаунт?</span> <Link to="/login">Войти</Link>
+      </div>
     </div>
   );
 };
