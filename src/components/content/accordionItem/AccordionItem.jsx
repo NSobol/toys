bdb7d8f31b8294ab784c052785918b045aea0b0f
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-05-23 08:28:00 +0300

    Добавлена стилизация аккордеону

--- src/components/content/accordionItem/AccordionItem.jsx
+++ src/components/content/accordionItem/AccordionItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ReactComponent as Arrow } from './../../../images/right_arrow.svg';
+import { ReactComponent as Arrows } from './../../../images/right_arrow_rotated.svg';
 import s from './accordionItem.module.css';
 
 export const AccordionItem = ({ item, index, toggle, opened }) => {
@@ -8,7 +10,9 @@ export const AccordionItem = ({ item, index, toggle, opened }) => {
   return (
     <div className={s.accordionItem} key={item.title} onClick={toggle}>
       <div className={s[`${isTitleActive}`]}>
-        <div className={s.icon}>{opened === index ? '-' : '+'}</div>
+        <div className={s.icon}>
+          {opened ? <Arrows /> : <Arrow />}
+        </div>
         <h2 className={s.title}>{item.title}</h2>
       </div>
       <div className={s[`${isActive}`]}>{item.content}</div>
