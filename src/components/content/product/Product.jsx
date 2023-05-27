b0dee01cd87108b1cfcee850947555607dae54d4
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-05-27 18:41:40 +0300

    Исправлен баг количества отзывов и стилизов. акция

--- src/components/content/product/Product.jsx
+++ src/components/content/product/Product.jsx
@@ -1,23 +1,20 @@
-import React, { useContext } from "react";
-import s from "./Product.module.css";
-import { Modal } from "./../../modal/Modal";
-import { FormReview } from "../../formReview/FormReview";
-import { ProductsContext } from "../../../context/productsContext";
-import { Rating } from "../rating/Rating";
-// import { getCorrectWordEnding } from "../../../utils/function";
+import React, { useContext } from 'react';
+import s from './Product.module.css';
+import { Modal } from './../../modal/Modal';
+import { FormReview } from '../../formReview/FormReview';
+import { ProductsContext } from '../../../context/productsContext';
+import { Rating } from '../rating/Rating';
+import { getCorrectWordEnding } from '../../../utils/function';
 
-export const Product = ({ product, setProduct, reviews}) => {
+export const Product = ({ product, setProduct, reviews }) => {
   const { active, setActive } = useContext(ProductsContext);
-//   const arrReview = product?.reviews ?? [];
-  console.log(product.reviews.length);
-    // console.log(arrReview.length);
-
+  console.log(product.reviews?.length);
   const getDiscountPrice = (discount, price) => {
     return (price - Math.floor((price * discount) / 100)).toFixed(0);
   };
 
   const productRating = (reviews) => {
-    if (!reviews || !reviews.length) {
+    if (!reviews || !reviews?.length) {
       return 0;
     }
     const res = reviews.reduce((acc, el) => (acc += el.rating), 0);
@@ -38,10 +35,10 @@ export const Product = ({ product, setProduct, reviews}) => {
           <h2 className={s.product__title}>{product.name}</h2>
           <div className={s.rating}>
             <Rating />
-            {/* <span>
-              {product?.reviews.length}
-              {getCorrectWordEnding(product?.reviews.length, "отзыв")}
-            </span> */}
+            <div>
+              {product.reviews?.length}
+              {getCorrectWordEnding(product.reviews?.length, 'отзыв')}
+            </div>
           </div>
           <p className={s.rightBarP}>Описание: {product.description}</p>
           <p className={s.rightBarP}>Ед. измерения: {product.wight}</p>
@@ -67,7 +64,7 @@ export const Product = ({ product, setProduct, reviews}) => {
                     </span>
                   )}
                 </div>
-              }{' '}
+              }
             </b>
           </div>
           <div className={s.inCase}>
