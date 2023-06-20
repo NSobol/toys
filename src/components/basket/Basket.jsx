814a7a70f0790e9c8b7599554542ac7b2464576f
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-06-20 13:18:41 +0300

    Корзина.Продолжение2

--- src/components/basket/Basket.jsx
+++ src/components/basket/Basket.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { ProductsContext } from '../../context/productsContext';
 import { getDiscountPrice } from './../../utils/function';
+import s from './basket.module.css'
 
 export const Basket = () => {
   const { basket, setBasket } = useContext(ProductsContext);
-	const [count, setCount] = useState({count: 0});
+
   const getRemoveItemFromBasket = (id) => {
     const gds = basket.filter((el) => el.id !== id);
     if (basket.length > 1) {
@@ -14,14 +15,36 @@ export const Basket = () => {
       setBasket(gds);
       localStorage.removeItem('basket');
     }
+	};
+	const minus = (id) => {
+    setBasket((prev) => {
+      const test = prev.filter((el) => el.id === id);
+      if (test.length) {
+        return prev.map((el) => {
+          if (el.id === id) {
+            el.cnt--;
+          }
+          return el;
+        });
+      }
+    });
   };
-  //  const minus = () => {
-  //    setCount((prev) => prev - 1);
-  //  };
 
-  //  const plus = () => {
-  //    setCount((prev) => prev + 1);
-  //  };
+  const plus = (id) => {
+     setBasket((prev) => {
+      const test = prev.filter((el) => el.id === id);
+      if (test.length) {
+        return prev.map((el) => {
+          if (el.id === id) {
+            el.cnt++;
+          }
+          return el;
+        });
+      }
+    });
+  };
+
+  localStorage.setItem('basket', JSON.stringify(basket));
 
   return (
     <div className='container'>
@@ -29,30 +52,30 @@ export const Basket = () => {
 
       <div className='blockLeft'>
         {basket.map((item) => {
-          let count = item.cnt;
           return (
             <div key={item.product._id}>
               <img src={item.product.pictures} alt='' />
               <p>{item.product.name}</p>
+              <p>Единицы измерения: {item.product.wight}</p>
               <p>
                 {getDiscountPrice(item.product.discount, item.product.price)}
                 &nbsp;p
               </p>
-              <div className='inCaseControls'>
+              <div className={s.inCaseControls}>
                 <button
-                  className='inCaseMinus'
+                  className={s.inCaseMinus}
                   disabled={item.cnt <= 0 ? true : false}
-                  onClick={(count) => count - 1}
+                  onClick={() => minus(item.id)}
                 >
-                  <span className='minusText'>-</span>
+                  <span className={s.minusText}>-</span>
                 </button>
-                <span className='caseText'>{count}шт</span>
+                <span className={s.caseText}>{item.cnt}</span>
                 <button
-                  className='inCasePlus'
-                  onClick={(count) => count + 1}
-                  disabled={item.cnt === item.stock ? true : false}
+                  className={s.inCasePlus}
+                  onClick={() => plus(item.id)}
+                  disabled={item.cnt !== item.stock ? false : true}
                 >
-                  <span className='plusText'>+</span>
+                  <span className={s.plusText}>+</span>
                 </button>
               </div>
               <div className='del'>
