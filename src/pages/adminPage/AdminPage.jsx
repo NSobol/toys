6ed90b238cf8b413cc8c9be0cbb33f879443af77
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-06-20 15:12:33 +0300

    Изменения верстки

--- src/pages/adminPage/AdminPage.jsx
+++ src/pages/adminPage/AdminPage.jsx
@@ -11,32 +11,32 @@ export const AdminPage = () => {
           'Content-Type': 'application/json',
         },
       }
-	).then((res) => {
-		
-
-		
-		return res.ok
-      ? res.json()
-      : res.json().then((data) => Promise.reject(data));
-
-	});
-	  console.log(result.data[0].metrics)
-	  return result;
+    ).then((res) => {
+      return res.ok
+        ? res.json()
+        : res.json().then((data) => Promise.reject(data));
+    });
+    return result;
   };
 
   return (
     <div>
-      <ComeBack />
-      <h1>Страница администратора</h1>
-      <div>
-        <a href='https://metrika.yandex.ru/dashboard?id=94010807'>
-          Отчет Яндекс Метрики
-        </a>
-        <button onClick={()=>getAnalytics()}>Получить аналитику</button>
-      </div>
-      {/* <div>{()=>getAnalytics()}</div> */}
-      <div>
-        <AdminHeaderIcons />
+      <div className='container'>
+        <ComeBack />
+        <h1>Страница администратора</h1>
+        <div className='contentAdminPage'>
+          <div>
+            <p>Добавление товара: &nbsp;</p>
+            <AdminHeaderIcons />
+          </div>
+          <div>
+            {/* <a href='https://metrika.yandex.ru/dashboard?id=94010807'>
+            Отчет Яндекс Метрики
+          </a> */}
+            <p>Статистика посещений: &nbsp;</p>
+            <button onClick={() => getAnalytics()}>Получить аналитику</button>
+          </div>
+        </div>
       </div>
     </div>
   );
